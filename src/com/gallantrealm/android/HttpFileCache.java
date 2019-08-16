6377a3ad385e7094c1d9e63ef89cae7b2605b6ab
BJ Owings <bj@gallantrealm.com>
2019-08-16 15:06:42 -0700

    Add if-modified-since support to HttpFileCache

--- src/com/gallantrealm/android/HttpFileCache.java
+++ src/com/gallantrealm/android/HttpFileCache.java
@@ -9,35 +9,58 @@ import java.net.URL;
 import android.content.Context;
 
 /**
- * Provides caching of files obtained via Https.  Files are kept in the app internal file cache.
+ * Provides caching of files obtained via Https. Files are kept in the app internal file cache.
  */
 public final class HttpFileCache {
 
 	public static File getFile(String urlString, Context context) {
-		String urlFileName = urlString.replace("/", "_").replace(":","_").replace("%","_");
+		boolean fileCached = false;
+		String urlFileName = urlString.replace("/", "_").replace(":", "_").replace("%", "_");
 		File file = new File(context.getCacheDir(), urlFileName);
-		if (!file.exists()) {
-			try {
-				HttpURLConnection connection = (HttpURLConnection)(new URL(urlString)).openConnection();
-				InputStream inputStream = connection.getInputStream();
-				OutputStream outputStream = new FileOutputStream(file);
-				byte[] buffer = new byte[1024];
-				int len = 0;
-				while (len >= 0) {
-					len = inputStream.read(buffer);
-					if (len > 0) {
-						outputStream.write(buffer, 0, len);
-					}
+		try {
+			HttpURLConnection connection = (HttpURLConnection) (new URL(urlString)).openConnection();
+			if (file.exists()) {
+				fileCached = true;
+				long date = file.lastModified();
+				connection.setIfModifiedSince(date);
+			}
+			int response = ((HttpURLConnection) connection).getResponseCode();
+			if (response == HttpURLConnection.HTTP_NOT_FOUND) {
+				System.out.println(urlString+" is not found");
+				if (fileCached) {
+					System.out.println("deleting cached version");
+					file.delete();
 				}
-				outputStream.close();
-				inputStream.close();
-			} catch (Exception e) {
-				System.err.println(e);
 				return null;
 			}
-		} else {
-			System.out.println("HttpFileCache: "+urlString+" retrieved from file cache.");
-			// need to check for stale files
+			if (response == HttpURLConnection.HTTP_UNAVAILABLE) {
+				System.out.println(urlString+" is not available");
+				if (fileCached) {
+					return file;
+				} else {
+					return null;
+				}
+			}
+			if (response == HttpURLConnection.HTTP_NOT_MODIFIED) {
+				System.out.println(urlString+" has not modified.  Using cached version.");
+				return file;
+			}
+			System.out.println("Downloading and caching "+urlString);
+			InputStream inputStream = connection.getInputStream();
+			OutputStream outputStream = new FileOutputStream(file);
+			byte[] buffer = new byte[1024];
+			int len = 0;
+			while (len >= 0) {
+				len = inputStream.read(buffer);
+				if (len > 0) {
+					outputStream.write(buffer, 0, len);
+				}
+			}
+			outputStream.close();
+			inputStream.close();
+		} catch (Exception e) {
+			System.err.println(e);
+			return null;
 		}
 		return file;
 	}
